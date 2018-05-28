901ba3893c14731f12f6dc3dd60f6814d5981dd1
Faheem Kamal <faheem.kamal.a@gmail.com>
2018-05-29 03:03:16 +0600

    Revert "fix sepolicy error"
    This reverts commit ce036f9fc6c09819a1254136e323942623f0f5d1.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -163,8 +163,8 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 USE_OPENGL_RENDERER := true
 
 # SELinux
--include device/qcom/sepolicy/sepolicy.mk
--include device/qcom/sepolicy/legacy-sepolicy.mk
+include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/legacy-sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Shims
